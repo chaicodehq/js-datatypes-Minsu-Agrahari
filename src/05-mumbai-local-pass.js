f58d3d8e40dc1c6d0330685ff3f35a925240d220
Minsu-Agrahari <cmminsuagrahari@gmail.com>
2026-02-14 04:21:41 +0530

    Implement validation and pass generation logic in Mumbai Local Pass function

--- src/05-mumbai-local-pass.js
+++ src/05-mumbai-local-pass.js
@@ -8,11 +8,14 @@
  * Rules:
  *   - passenger object mein required fields: name, from, to, classType
  *   - classType must be "first" ya "second" (case-insensitive check)
+ * 
  *   - Pass ID generate karo:
  *     classType ka first char uppercase + from ke pehle 3 letters uppercase
  *     + to ke pehle 3 letters uppercase
+ * 
  *     Example: "first", "dadar", "andheri" => "F" + "DAD" + "AND" = "FDADAND"
  *   - Output format using template literal:
+ * 
  *     Line 1: "MUMBAI LOCAL PASS"
  *     Line 2: "---"
  *     Line 3: "Name: <NAME IN UPPERCASE>"
@@ -20,8 +23,10 @@
  *     Line 5: "To: <To in Title Case>"
  *     Line 6: "Class: <FIRST or SECOND>"
  *     Line 7: "Pass ID: <PASSID>"
+ * 
  *   - Title Case = first letter uppercase, rest lowercase
  *   - Lines are separated by \n (newline)
+ * 
  *   - Hint: Use template literals, slice(), toUpperCase(), toLowerCase(),
  *     charAt(), typeof
  *
@@ -43,4 +48,25 @@
  */
 export function generateLocalPass(passenger) {
   // Your code here
+
+  if(typeof passenger !== "object" || passenger === null) return "INVALID PASS";
+  if(passenger.name === undefined ||
+      passenger.name === "" ||
+      passenger.from === null ||
+      passenger.from === undefined ||
+      passenger.to === null || 
+      passenger.to === undefined 
+  )return "INVALID PASS";
+
+  if(passenger.classType.toLowerCase() !== "first" && passenger.classType.toLowerCase() !== "second") return "INVALID PASS";
+
+  let name = passenger.name.toUpperCase();
+  let from = passenger.from.charAt(0).toUpperCase() + passenger.from.slice(1).toLowerCase();
+  let to = passenger.to.charAt(0).toUpperCase() + passenger.to.slice(1).toLowerCase();
+  let classType = passenger.classType.toUpperCase();
+
+  let passId = classType.charAt(0)+ from.slice(0, 3) + to.slice(0,3);
+  passId = passId.toUpperCase();
+
+  return(`MUMBAI LOCAL PASS\n---\nName: ${name}\nFrom: ${from}\nTo: ${to}\nClass: ${classType}\nPass ID: ${passId}`);
 }
